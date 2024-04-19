54a6c979dbb41d9e8866766eddd1e74229612e21
LucianoMarti23 <luciano.marti12@hotmail.com>
2024-04-19 13:06:12 -0300

    Refactor

--- src/main/java/com/example/demo/service/ProfesionalServiceImpl.java
+++ src/main/java/com/example/demo/service/ProfesionalServiceImpl.java
@@ -1,155 +1,95 @@
 package com.example.demo.service;
-import com.example.demo.dto.PacienteDTO;
 import com.example.demo.dto.ProfesionalDTO;
-import com.example.demo.exceptions.PacienteAlreadyExist;
 import com.example.demo.model.profesionales;
 import com.example.demo.repository.I_ProfesionalesRepository;
 import com.example.demo.serviceInterface.I_ProfesionalService;
 import com.example.demo.utils.MapUtils;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class ProfesionalServiceImpl implements I_ProfesionalService {
 
-    private final I_ProfesionalesRepository profesionalesRepository;
+  private final I_ProfesionalesRepository profesionalesRepository;
 
+  @Override
+  public List<ProfesionalDTO> getAllProfesional() {
 
+    return MapUtils.getAll(profesionalesRepository.findAll(), ProfesionalDTO.class);
+  }
 
-    private static profesionales getProfesional(ProfesionalDTO profesionalDTO) {
+  @Override
+  public ProfesionalDTO getProfesionalbyDNI(int dni) {
+    return profesionalesRepository
+        .findByDniProfesional(dni)
+        .map(
+            existingProfesional ->
+                MapUtils.mapEntityToDTO(existingProfesional, ProfesionalDTO.class))
+        .orElseThrow(
+            () ->
+                new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Profesional with ID " + dni + " not found"));
+  }
 
-        profesionales nuevoProfesional = new profesionales();
+  public void guardarProfesional(ProfesionalDTO profesionalDto) {
+    Optional<profesionales> optionalPaciente =
+        profesionalesRepository.findByDniProfesional(profesionalDto.getDni_profesional());
 
-        nuevoProfesional.setNombre(profesionalDTO.getNombre());
-        nuevoProfesional.setCelular(profesionalDTO.getCelular());
-        nuevoProfesional.setEmail(profesionalDTO.getEmail());
-        nuevoProfesional.setApellido(profesionalDTO.getApellido());
-        nuevoProfesional.setTitulo_profesional(profesionalDTO.getTitulo_profesional());
-        nuevoProfesional.setMatricula(profesionalDTO.getMatricula());
-        nuevoProfesional.setDniProfesional(profesionalDTO.getDni_profesional());
+    optionalPaciente.ifPresent(
+        paciente -> {
+          throw new ResponseStatusException(HttpStatus.CONFLICT, "Paciente already exists.");
+        });
 
-        return nuevoProfesional;
+    profesionales pacienteEntity = MapUtils.mapDtoToEntity(profesionalDto, profesionales.class);
+    profesionalesRepository.save(pacienteEntity);
+  }
+
+  @Override
+  public void updatearProfesional(int dni, ProfesionalDTO nuevoProfesional) {
+    {
+      Optional<profesionales> optionalPacienteExisting =
+          profesionalesRepository.findByDniProfesional(dni);
+
+      profesionales existingPaciente =
+          optionalPacienteExisting.orElseThrow(
+              () ->
+                  new ResponseStatusException(
+                      HttpStatus.NOT_FOUND, "Paciente with ID " + dni + " not found"));
+
+      Optional<profesionales> optionalPacienteDtoExisting =
+          profesionalesRepository.findByDniProfesional(nuevoProfesional.getDni_profesional());
+
+      if (optionalPacienteDtoExisting.isPresent()
+          && !optionalPacienteDtoExisting.get().equals(existingPaciente)) {
+        throw new ResponseStatusException(HttpStatus.CONFLICT, "Paciente with DNI already exists");
+      }
+
+      existingPaciente.setApellido(nuevoProfesional.getApellido());
+      existingPaciente.setNombre(nuevoProfesional.getNombre());
+      existingPaciente.setDniProfesional(nuevoProfesional.getDni_profesional());
+      existingPaciente.setMatricula(nuevoProfesional.getMatricula());
+      existingPaciente.setCelular(nuevoProfesional.getCelular());
+      existingPaciente.setTitulo_profesional(nuevoProfesional.getTitulo_profesional());
+      existingPaciente.setEmail(nuevoProfesional.getEmail());
+
+      profesionalesRepository.save(existingPaciente);
     }
+  }
 
-    private ProfesionalDTO mapProfesionalToDTO(profesionales profesional) {
+  @Override
+  public void eliminarProfesional(int dni) {
+    Optional<profesionales> optionalPaciente = profesionalesRepository.findByDniProfesional(dni);
 
-        ProfesionalDTO profesionalDTO = new ProfesionalDTO();
-        profesionalDTO.setNombre(profesional.getNombre());
-        profesionalDTO.setApellido(profesional.getApellido());
-        profesionalDTO.setCelular(profesional.getCelular());
-        profesionalDTO.setEmail(profesional.getEmail());
-        profesionalDTO.setDni_profesional(profesional.getDniProfesional());
-        profesionalDTO.setTitulo_profesional(profesional.getTitulo_profesional());
-        profesionalDTO.setMatricula(profesional.getMatricula());
+    profesionales existspaciente = optionalPaciente.orElseThrow(() ->
+            new ResponseStatusException(HttpStatus.NOT_FOUND , "Paciente with DNI NOT FOUND"));
 
-
-        return profesionalDTO;
-
-    }
-
-
-
-
-
-
-
-
-    @Override
-    public List<?> getAllProfesional() {
-        try{
-           List <profesionales> listProfesionales = profesionalesRepository.findAll();
-
-
-            return  MapUtils.getAll(listProfesionales, ProfesionalDTO.class);
-        }catch (Exception e) {
-
-            throw new RuntimeException("Failed to retrieve all Profesionales. Reason: " + e.getMessage());
-        }
-    }
-
-
-    @Override
-    public Optional<ProfesionalDTO> getProfesionalbyDNI(int dni) {
-        Optional<profesionales> optionalProfesional = profesionalesRepository.findByDniProfesional(dni);
-        if (optionalProfesional.isPresent()) {
-
-            profesionales profesional = optionalProfesional.get();
-            ProfesionalDTO profesional_DTO = mapProfesionalToDTO(profesional);
-            return Optional.of(profesional_DTO);
-
-
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente with ID " + dni + " not found");
-        }
-    }
-
-    @Override
-    public void guardarProfesional(ProfesionalDTO profesional) {
-        Optional<profesionales> optionalPaciente = profesionalesRepository.findByDniProfesional(profesional.getDni_profesional());
-
-        if (optionalPaciente.isPresent()) {
-            throw new PacienteAlreadyExist("Profesional with ID " + profesional.getDni_profesional() + " already exists.");
-        }
-
-        try {
-            profesionales profesionalNew = getProfesional(profesional);
-            profesionalesRepository.save(profesionalNew);
-        } catch (PacienteAlreadyExist e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        }
-    }
-
-    @Override
-    public void updatearProfesional(int dni, ProfesionalDTO nuevoProfesional) {
-        Optional<profesionales> optionalProfesionalExisting = profesionalesRepository.findByDniProfesional(dni);
-        Optional<profesionales> optionalProfesionalDtoExisting = profesionalesRepository.findByDniProfesional(nuevoProfesional.getDni_profesional());
-
-        profesionales existingProfesional = optionalProfesionalExisting.orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND, "Profesional with ID " + dni + " not found"));
-
-
-        if (optionalProfesionalDtoExisting.isEmpty()) {
-
-            existingProfesional.setApellido(nuevoProfesional.getApellido());
-            existingProfesional.setNombre(nuevoProfesional.getNombre());
-            existingProfesional.setMatricula(nuevoProfesional.getMatricula());
-            existingProfesional.setDniProfesional(nuevoProfesional.getDni_profesional());
-            existingProfesional.setEmail(nuevoProfesional.getEmail());
-            existingProfesional.setCelular(nuevoProfesional.getCelular());
-            existingProfesional.setTitulo_profesional(nuevoProfesional.getTitulo_profesional());
-
-
-            profesionalesRepository.save(existingProfesional);
-
-
-        }
-
-
-    }
-
-
-
-
-
-
-
-
-    @Override
-    public void eliminarProfesional(int dni) {
-        Optional<profesionales> optionalProfesional = profesionalesRepository.findByDniProfesional(dni);
-        if (optionalProfesional.isPresent()) {
-            profesionalesRepository.deleteById(optionalProfesional.get().getId());
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Profesional with ID " + dni + " not found");
-        }
-
-    }
+    profesionalesRepository.delete(existspaciente);
+  }
 }
--- src/main/java/com/example/demo/serviceInterface/I_ProfesionalService.java
+++ src/main/java/com/example/demo/serviceInterface/I_ProfesionalService.java
@@ -2,24 +2,16 @@ package com.example.demo.serviceInterface;
 
 import com.example.demo.dto.ProfesionalDTO;
 import java.util.List;
-import java.util.Optional;
 
 public interface I_ProfesionalService {
 
-    List<?> getAllProfesional();
+  List<ProfesionalDTO> getAllProfesional();
 
-    Optional<ProfesionalDTO> getProfesionalbyDNI(int dni);
+  ProfesionalDTO getProfesionalbyDNI(int dni);
 
+  void guardarProfesional(ProfesionalDTO profesional);
 
-    void guardarProfesional(ProfesionalDTO profesional);
+  void updatearProfesional(int dni, ProfesionalDTO nuevoProfesional);
 
-
-    void updatearProfesional(int dni , ProfesionalDTO nuevoProfesional);
-
-    void eliminarProfesional(int dni);
-
-
-
-
-
+  void eliminarProfesional(int dni);
 }
